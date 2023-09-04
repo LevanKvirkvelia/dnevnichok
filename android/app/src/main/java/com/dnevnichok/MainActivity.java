493ad7379615a58160165462333db902b7c0b967
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2023-09-04 11:49:02 -0600

    puk

--- android/app/src/main/java/com/dnevnichok/MainActivity.java
+++ android/app/src/main/java/com/dnevnichok/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dnevnichok;
 
 import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -19,7 +20,8 @@ public class MainActivity extends ReactActivity {
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    RNBootSplash.init(this, R.style.BootTheme); // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
   }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
--- android/app/src/main/java/com/dnevnichok/MainApplication.java
+++ android/app/src/main/java/com/dnevnichok/MainApplication.java
@@ -1,5 +1,6 @@
 package com.dnevnichok;
 
+import com.microsoft.codepush.react.CodePush;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -17,6 +18,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
+        }
+
+              @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
 
         @Override
